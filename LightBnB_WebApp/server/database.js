cb684f87c8a4140791f5f4e8e35b21332bdc908d
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-04-02 16:47:01 +0000

    Modify code to take user's reservation from database instead of in memory

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -78,7 +78,20 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  let reservations;
+  return pool.query(`
+    SELECT properties.*, reservations.*, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id 
+    WHERE reservations.end_date < now()::date 
+    AND reservations.guest_id = $1
+    GROUP BY reservations.id, properties.id
+    ORDER BY reservations.start_date
+    LIMIT $2;`, [guest_id, limit]).then(res => {
+      reservations = [res.rows[0]]
+      return reservations;
+    })
 }
 exports.getAllReservations = getAllReservations;
 
